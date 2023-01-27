ecabdcd241a24ea54de17635015ff960483adb11
olojo-99 <benjamin.olojo2@mail.dcu.ie>
2023-01-27 09:47:00 +0000

    Adding caching to flask app

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ import os
 import openai
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_caching import Cache
 from segment import segment_transcript
 from rewrite import rewrite_segment
 from summarise import sub_summarise
@@ -9,18 +10,24 @@ from overall import meta_summarise
 from video_info import manual_transcript
 from parallel import thread_runner
 
-
-# OpenAI API key hidden in environment variable file
+config = {
+    "DEBUG": True,
+    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 300
+}
 
 app = Flask(__name__)  # creating instance of flask app with same name as file
 CORS(app)  # activate cors
 
+app.config.from_mapping(config)
+cache = Cache(app) # only one cookie per client
 
 # GET request from Frontend will contain video id in request vid_id
 # Full video URL needs to be parsed on frontend
 
 # Return Timestamp/Segment pairs + Overall summary
 @app.route("/summarise/<vid_id>", methods=["GET"])
+@cache.cached(timeout=300) # caching summarisation results
 def transcript_summary(vid_id):
 
     # vid_id = "Unl1jXFnzgo"  # EXAMPLE MANUAL TRANSCRIPT ID
@@ -40,6 +47,9 @@ def transcript_summary(vid_id):
     # Generate overall summary based on summarised segments
     ovr_summary = meta_summarise("\n".join(vid_segments.values()))
 
+    # map video id to overall summary in cache
+    cache.set(vid_id, ovr_summary)
+
     return jsonify(
         segments=vid_segments,
         overall=ovr_summary)
@@ -49,9 +59,12 @@ def transcript_summary(vid_id):
 # Links will be based on Fact Extraction on summarised segments
 
 # Returns list of Google links
-@app.route("/links", methods=["GET"])
-def ir_links():
-    return "Information Retrieval Links"
+@app.route("/links/<vid_id>", methods=["GET"])
+@cache.cached(timeout=300) # caching IR results
+def ir_links(vid_id):
+    # get cached overall summary for video
+    ovr_summary = cache.get(vid_id)
+    return jsonify(ovr_summary)
 
 
 # Run application
