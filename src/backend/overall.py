961b9605a5c87eb7252b24c6b92024ac976fdf7c
Przemyslaw Majda <przemyslaw.majda2@mail.dcu.ie>
2023-02-12 18:46:57 +0000

    small change to overall prompt

--- src/backend/overall.py
+++ src/backend/overall.py
@@ -1,21 +1,23 @@
 from completion import gpt3_completion
 
 # generate prompt for summarising overall text
+
+
 def meta_prompt(text):
     # return f"Create a complete overall summary of the following combined video segments.\n\n combined video segments: \"\"\"\n{text}\n\"\"\"\n"
-    return f"The following is a combination of video extract summaries. Rewrite this as a structured, clear summary.\n\nCombined Transcript Extracts: \"\"\"\n{text}\n\"\"\"\n"
+    return f"The following is a combination of video extract summaries. Rewrite this as a structured, clear summary that keeps some of the main facts and keeps the text well structured.\n\nCombined Transcript Extracts: \"\"\"\n{text}\n\"\"\"\n"
 
 
 # invoke func for querying gpt-3 and summarise overall text
 def meta_summarise(text):
     params = {
-        "model":"text-davinci-003",
-        "prompt":meta_prompt(text),
-        "temp":0.6,
-        "tokens":500,
-        "top_p":1,
-        "freq_pen":0,
-        "pres_pen":0
+        "model": "text-davinci-003",
+        "prompt": meta_prompt(text),
+        "temp": 0.6,
+        "tokens": 500,
+        "top_p": 1,
+        "freq_pen": 0,
+        "pres_pen": 0
     }
 
     # summarise overall text with gpt3
