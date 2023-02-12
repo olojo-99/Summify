1a54536c003004127c60c37c06212d273968821e
Przemyslaw Majda <przemyslaw.majda2@mail.dcu.ie>
2023-02-12 18:17:44 +0000

    minor styling changes

--- src/backend/summarise.py
+++ src/backend/summarise.py
@@ -1,9 +1,11 @@
 from completion import gpt3_completion
 
 # generate prompt for summarising segments
+
+
 def sub_prompt(extract):
-    #return f"Summarise the following video segment.\n\nvideo segment: \"\"\"\n{extract}\n\"\"\"\n"
-    return f"The following is an extract from a video transcript. Rewrite this as a structured, clear summary.\n\nVideo Transcript Extract: \"\"\"\n{extract}\n\"\"\"\n"
+    # return f"Summarise the following video segment.\n\nvideo segment: \"\"\"\n{extract}\n\"\"\"\n"
+    return f"The following is an extract from a video transcript. Rewrite this as a structured, clear summary. Focus on important facts while keeping the overall structure of the text.\n\nVideo Transcript Extract: \"\"\"\n{extract}\n\"\"\"\n"
     # return f"Summarise the following extract from a video transcript, focusing on important facts while still keeping the overall structure of the text.\n\nTranscript: \"\"\"\n{extract}\n\"\"\"\n"
 
 
