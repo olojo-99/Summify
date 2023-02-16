c47bee130088c8f488878306a52d913c47de1e9c
olojo-99 <benjamin.olojo2@mail.dcu.ie>
2023-02-16 20:05:26 +0000

    Adding error handling for failed GPT-3 calls

--- src/backend/completion.py
+++ src/backend/completion.py
@@ -7,20 +7,29 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 def gpt3_completion(model, prompt, temp, top_p, tokens, freq_pen, pres_pen):
-    # try:
-    response = openai.Completion.create(
-        engine=model,
-        prompt=prompt,
-        temperature=temp,
-        max_tokens=tokens,
-        top_p=top_p,
-        frequency_penalty=freq_pen,
-        presence_penalty=pres_pen
-    )
+    # make up to 3 GPT-3 calls
+    max_retry , retry = 3, 0
+    while True:
+        try:
+            response = openai.Completion.create(
+                engine=model,
+                prompt=prompt,
+                temperature=temp,
+                max_tokens=tokens,
+                top_p=top_p,
+                frequency_penalty=freq_pen,
+                presence_penalty=pres_pen
+            )
 
-    # return gpt3 text response
-    # need to remove unnecessary chars from response
-    return (response.choices[0].text.strip("\n"))
+            # return gpt3 text response
+            return (response.choices[0].text.strip("\n"))
 
-    # except:
-    #     return "Error generating GPT-3 Completion\n"
+        except Exception as error:
+            # make another attempt at the API call
+            retry += 1
+            # check number of attempts
+            if retry < max_retry:
+                sleep(2)
+                continue # return to start of call
+            else:
+                return "Error generating GPT-3 Completion\n"
