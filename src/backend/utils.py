b22e4263711089e0415375ee7736856607aedcd3
olojo-99 <benjamin.olojo2@mail.dcu.ie>
2023-02-22 15:09:33 +0000

    Adding error handling module

---
+++ src/backend/utils.py
@@ -0,0 +1,40 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+
+# Determine whether transcript is written manually or auto-generated
+def auto_transcript(vid_id):
+    transcript_list = YouTubeTranscriptApi.list_transcripts(vid_id)
+    transcript = transcript_list.find_transcript(['en'])
+
+    # make api call for each segment -> need to parallelise
+    if transcript.is_generated:
+        return True
+    else:
+        return False
+
+# Error handling for /summarise responses
+# Returns error message, error code
+def err_handler(response):
+    # check for 550 error
+    if response == "Max Length Exceeded":
+        return "Video exceeds maximum length", 550
+
+    elif response == "Unavailable":
+        return "Transcript Unavailable", 560
+
+    elif "GPT-3 Error" in response:
+        return "Error Generating GPT-3 Completion", 570
+
+    elif response == "Custom Search Engine Error":
+        return "Custom Search Engine Unavailable", 580
+
+    else:
+        return None # No errors found
+
+
+# Error checking for when GPT-3 is unavailable
+def completion_err(response):
+    if response == "Error generating GPT-3 Completion":
+        return "GPT-3 Error"
+
+    else:
+        return response
